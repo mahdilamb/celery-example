6016c0283e6b55aafc977844de108a1137d1591f
Mahdi Lamb <mahdilamb@gmail.com>
2025-11-25 14:27:21 +0000

    fix(tests): fix env for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
 import pytest
-from celery_example.tasks import app as celery_app
+from dotenv import dotenv_values
+import os
+
+
+def pytest_sessionstart(session):  # type: ignore
+    """Setup before tests start."""
+
+    # Update the environment with example env
+    os.environ["ENV"] = "TESTING"
+    for key, val in dotenv_values(".env.example").items():
+        os.environ[key] = val  # type: ignore
 
 
 @pytest.fixture(scope="session", autouse=True)
 def celery_config():
+    from celery_example.tasks import app as celery_app
+
     celery_app.conf.update(task_always_eager=True)
     return celery_app
